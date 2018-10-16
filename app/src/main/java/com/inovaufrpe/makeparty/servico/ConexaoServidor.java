08b8bd8e7648f6a1906210eda1bdac1aa2fda780
Paulo Andrade <pcdajr@gmail.com>
2018-10-16 18:49:16 -0300

    sync do Gson e começando classes de requisição

--- app/src/main/java/com/inovaufrpe/makeparty/servico/ConexaoServidor.java
+++ app/src/main/java/com/inovaufrpe/makeparty/servico/ConexaoServidor.java
@@ -1,10 +1,21 @@
 package com.inovaufrpe.makeparty.servico;
 
 import android.os.AsyncTask;
+import java.net.HttpURLConnection;
+
+
+import com.google.android.gms.common.images.WebImage;
+import com.inovaufrpe.makeparty.servico.UsuarioService;
 
 public class ConexaoServidor extends AsyncTask<String, String, String> {
     @Override
     protected String doInBackground(String... strings) {
         return null;
     }
+
+    public String criarJsonObjeto(Object objeto){
+        return gson.toJson(objeto);
+    }
+
+
 }
