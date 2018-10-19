35aaf5734cc319d18f5a8037a4029cbb2a4fa7a4
Paulo Andrade <pcdajr@gmail.com>
2018-10-19 03:18:41 -0300

    continuando a construção do metodo de requisição http
    Ainda estou tentando fazer ele dinamico, pra identificar qual o metodo da url quer usar

--- app/src/main/java/com/inovaufrpe/makeparty/servico/ConexaoServidor.java
+++ app/src/main/java/com/inovaufrpe/makeparty/servico/ConexaoServidor.java
@@ -4,12 +4,11 @@ import android.os.AsyncTask;
 import java.net.HttpURLConnection;
 
 
-import com.google.android.gms.common.images.WebImage;
-import com.inovaufrpe.makeparty.servico.UsuarioService;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.net.HttpURLConnection;
+
 import java.net.URL;
 import java.util.Objects;
 
@@ -19,27 +18,62 @@ public class ConexaoServidor extends AsyncTask<String, String, String> {
 
     private UsuarioService usuarioService = new UsuarioService();
 
-    @Override
-    protected String doInBackground(String... strings) {
-        return null;
-        // Este método é usado para exibir qualquer forma de progresso na interface do usuário,
-        // enquanto a tarefa ainda está em execução.
+
+//requisição HTTP
+    private String conectarUser(String... strings){
+        String jsonResposta = null;
+        try{
+            URL url = new URL(usuarioService.getUrlCriarUsuario());
+            HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+            conexao.setRequestMethod("POST");
+            conexao.addRequestProperty("Content-type", "application/json");
+
+            conexao.setDoOutput(true);
+            conexao.setDoInput(true);
+
+            PrintStream printStream = new PrintStream(conexao.getOutputStream());
+            printStream.println();
+
+            conexao.connect();
+
+            BufferedReader reader = new BufferedReader( new InputStreamReader( conexao.getInputStream()));
+
+            conexao.disconnect();
+            StringBuilder sbHtml = new StringBuilder();
+            String linha;
+
+            while( ( linha = reader.readLine() ) != null )
+            {
+                sbHtml.append (linha);
+            }
+            jsonResposta = sbHtml.toString();
+            reader.close();
+            printStream.close();
+            conexao.disconnect();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        usuarioService.setRespostaServidor(jsonResposta);
+
+        return jsonResposta;
     }
 
-   // @Override
-   // protected void onPreExecute(){
-        // Este passo é usado para configurar a tarefa, por exemplo, mostrando uma barra de progresso na interface do usuário.
-   // }
 
-   //@Override
-   // protected void onProgressUpdate () {
-        // Este método é usado para exibir qualquer forma de progresso na interface do usuário, enquanto a tarefa ainda está em execução.
-   // }
+//Thread para das requisições
+    @Override  // Este método é usado para exibir qualquer forma de progresso na interface do usuário, enquanto a tarefa ainda está em execução.
+    protected String doInBackground(String... strings) {
+        String jsonResposta = null;
 
-   // @Override
-   // protected void onPostExecute(){
-        // // O resultado da execução em background é passado para este passo como um parâmetro.
+        jsonResposta = conectarUser(strings);
+        return jsonResposta;
+    }
 
+    //@Override // Este passo é usado para configurar a tarefa, por exemplo, mostrando uma barra de progresso na interface do usuário.
+    //protected void onPreExecute(){
     //}
 
+    //@Override //O resultado da execução em background é passado para este passo como um parâmetro.
+    //protected void onPostExecute(){
+    //}
 }
--- app/src/main/java/com/inovaufrpe/makeparty/servico/UsuarioService.java
+++ app/src/main/java/com/inovaufrpe/makeparty/servico/UsuarioService.java
@@ -1,35 +1,37 @@
 package com.inovaufrpe.makeparty.servico;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+
+
+
 
 public class UsuarioService {
 
     private static final String URL_BASE = "https://makepartyserver.herokuapp.com/";
-    private static final String CRIAR_USUARIO = URL_BASE + "users";
-    private static final String AUTENTICAR_USUARIO = URL_BASE + "users/authenticate";
-    private static final String ATUALIZAR_TOKEN = URL_BASE + "users/refresh-token";
-    private static final String METODO_GET = "GET";
-    private static final String METODO_POST = "POST";
-    private static final String METODO_PUT = "PUT";
-    private static final String METODO_DELETAR = "DELETE";
+    private static final String URL_CRIAR_USUARIO = URL_BASE + "users";
+    private static final String URL_AUTENTICAR_USUARIO = URL_BASE + "users/authenticate";
+    private static final String URL_ATUALIZAR_TOKEN = URL_BASE + "users/refresh-token";
     private Gson gson = new Gson();
+
+
+
     private String respostaServidor;
+    private ConexaoServidor conexaoServidor = new ConexaoServidor();
 
 
     public UsuarioService() {
+    } //CONSTRUTOR
+
+    public static String getUrlCriarUsuario() {
+        return URL_CRIAR_USUARIO;
     }
 
-    public static String getCriarUsuario() {
-        return CRIAR_USUARIO;
+    public static String getUrlAutenticarUsuario() {
+        return URL_AUTENTICAR_USUARIO;
     }
 
-    public static String getAutenticarUsuario() {
-        return AUTENTICAR_USUARIO;
-    }
-
-    public static String getAtualizarToken() {
-        return ATUALIZAR_TOKEN;
+    public static String getUrlAtualizarToken() {
+        return URL_ATUALIZAR_TOKEN;
     }
 
     public String getRespostaServidor() {
@@ -40,16 +42,15 @@ public class UsuarioService {
         this.respostaServidor = respostaServidor;
     }
 
-
-
-
-    public String criarJsonObjeto(Object objeto) {
+    //converte um objeto para json
+    public String criarJson(Object objeto) {
         return gson.toJson(objeto);
     }
 
+    //método que usa a requisição http implementada em conexaoServidor para criar usuário
     public void criarUsuario(Object objeto) {
-        String novoJson = criarJsonObjeto(objeto) ;
-       // conexaoServidor.execute(novoJson, CRIAR_USUARIO, METODO_POST);
+        String novoJson = criarJson(objeto) ;
+        conexaoServidor.execute(novoJson);
     }
 
 
